a860d9bbf9602e46e4b95238d32b9a390317aeb6
prosardar <prosardar@ya.ru>
2011-09-13 16:14:16 +0600

    added project

---
+++ 3DCity/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using Ingeo;
+
+namespace LandScape3D
+{
+    [ComVisible(true)]
+    [Guid("5b71e6e1-e7bb-4e61-be5d-60e46e9bfb22")]
+    public class Class1
+    {
+        Form1 f;
+
+        public Class1()
+        {
+
+        }
+
+        public void Init(IIngeoApplication Ingeo)
+        {
+          //  bdb = new BackupDb(Ingeo);
+
+            f = new Form1(Ingeo);
+
+            System.Windows.Forms.MessageBox.Show("Forms loaded");
+        }
+
+        public void Done()
+        {
+            f.Close();
+            f.Dispose();
+            f = null;
+        }
+    }
+}
